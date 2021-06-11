f4d39049a65345adffdfb7b09ed3183cb47bd7ec
Pyunghyuk Yoo <yoophi@gmail.com>
2021-06-11 19:17:29 +0900

    🩹 IMPROVE: eslint-prettier-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
   ],
   parserOptions: {
     project: './tsconfig.json',
